3ed61c898f9033d530522241867756ce81fa5994
StefanDbdn <StefanDbdn@users.noreply.github.com>
2018-09-05 22:16:30 +1000

    improved dogshit gameObject

--- GarbagePrototypeBackUp/Assets/Scripts/DogShit.cs
+++ GarbagePrototypeBackUp/Assets/Scripts/DogShit.cs
@@ -6,17 +6,32 @@ public class DogShit : MonoBehaviour {
 
 	float t;
 	float gameStartTime;
+
+    Rigidbody rb;
+
 	// Use this for initialization
 	void Start () {
 		gameStartTime = Time.timeSinceLevelLoad;
+
+        rb = GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		float t = Time.timeSinceLevelLoad - gameStartTime;
 		if (t > 5) {
-			
-			Destroy (gameObject);
+            rb.useGravity = false;
+            rb.isKinematic = true;
+            StartCoroutine(GoAway());
+			//Destroy (gameObject);
 		}
 	}
+
+    IEnumerator GoAway()
+    {
+        LeanTween.moveY(gameObject, gameObject.transform.position.y - 1, 2f);
+
+        yield return new WaitForSeconds(3f);
+        Destroy(gameObject);
+    }
 }
