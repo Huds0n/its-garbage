df8e28cd20a83e58e0c57326aaa4023131478139
StefanDbdn <StefanDbdn@users.noreply.github.com>
2018-09-08 20:24:41 +1000

    minor final scene improvements

---
+++ GarbagePrototypeBackUp/Assets/Scripts/finalscore.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class finalscore : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        LeanTween.scale(gameObject, new Vector3(1.5f, 1.5f, gameObject.transform.position.z), 1f).setLoopPingPong();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
